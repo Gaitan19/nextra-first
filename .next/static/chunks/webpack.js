9a9b36cf007bb2f52c6ca407a144c0ecf0eaceaf
kenley.gaitan <kenley.gaitan@smbssolutions.com>
2024-07-30 13:40:45 -0600

    feat: Add react-select dependency and update Components/card page
    The code changes add the react-select dependency to the package.json file and update the Components/card page in the pages-manifest.json file. This change allows for the usage of the react-select component in the project.
    
    These changes aim to enhance the project by adding the react-select dependency and updating the Components/card page to incorporate the new component.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b83859bbfba00b5f"; }
+/******/ 		__webpack_require__.h = function() { return "946fdbe749e8d768"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
